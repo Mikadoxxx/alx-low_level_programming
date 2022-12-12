1151c3cb8f1d45f00f2102d5067c9124ff33fd6c
Mikadoxxx <mikadoblaze289@gmail.com>
2022-12-12 08:45:52 -0800

    first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	float floatType;
 	double doubleType;
 	char charType;
-	sizeof('evaluates') : the size of a variable\n
+	sizeof(evaluates); : the size of a variable
 		printf("Size of int: %zu bytes\n", sizeof(intType));
 		printf("Size of float: %zu bytes\n", sizeof(floatType));
 		printf("Size of double: %zu bytes\n", sizeof(doubleType));
