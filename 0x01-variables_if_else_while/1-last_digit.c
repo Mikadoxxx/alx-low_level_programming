f1340a9404041406b6987b567fed81abc053bbc0
Mikadoxxx <mikadoblaze289@gmail.com>
2022-12-12 13:58:13 -0800

    final commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  *main -> assign a random number to the variable n each time it is executed
  *and print the last digit of the number stored in the variable n
- *Return: 0 
+ *Return: 0
  */
 int main(void)
 {
