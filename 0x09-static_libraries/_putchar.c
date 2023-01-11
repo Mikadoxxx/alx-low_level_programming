215d3e43011dc1fe13eb867cedcadcc4d1494868
Mikadoxxx <mikadoblaze289@gmail.com>
2023-01-11 00:18:30 -0800

    libraries

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -3,16 +3,14 @@
 
 
 /**
+ *	_putchar - writes the character c to stdout
  *
- *  * _putchar - writes the character c to stdout
+ *	@c: The character to print
  *
- *   * @c: The character to print
+ *	Return: On success 1.
  *
- *    *
- *
- *     * Return: On success 1.
- *
- *      * On error, -1 is returned, and errno is set appropriately*/
+ *	On error, -1 is returned, and errno is set appropriately
+ */
 
 int _putchar(char c)
 
